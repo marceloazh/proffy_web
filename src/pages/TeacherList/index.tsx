9f09014a1d33ec7f37609a2264d44f0b82a63b72
Marcelo Porto <marcelo.portos@gmail.com>
2020-08-09 22:38:48 -0300

    conclui front e prepara para API
    conclusão do front, com a criação de componentes novos, como Select, Textarea, assim como várias estilizações para que tudo fique bem formatado, tanto para web quanto no mobile.
    Acabei incluindo o axios e a pasta "services" neste mesmo commit, paciência!

--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
-
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -12,20 +12,35 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject" />
-                    </div>
-                
-                    <div className="input-block">
-                        <label htmlFor="week_day">Dia da semana</label>
-                        <input type="text" id="subject" />
-                    </div>
-                
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="subject" />
-                    </div>
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Histótia', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Inglês', label: 'Inglês' }
+                        ]}
+                    />
+                    <Select
+                        name="week_day"
+                        label="Dia da semana"
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input type="time" name="time" label="Hora" />
                 </form>
             </PageHeader>
 
--- src/pages/TearcherForm/index.tsx
+++ src/pages/TearcherForm/index.tsx
@@ -1,11 +1,110 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
+import Textarea from '../../components/Textarea';
+import Select from '../../components/Select';
+
+import warningIcon from '../../assets/images/icons/warning.svg';
+
+import './styles.css';
 
 function TeacherForm() {
+    const [scheduleItems, setScheduleItems] = useState([ //aqui abre variavel com useState, e usa desestruturação para pegar dois itens do vetor que é criado pelo useState. o primeiro é o que tem na variavel... a segunda é a função para mudar os dados do scheduleItems
+        { week_day: 1, from: '', to: '' },
+    ]);
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems, //por ter o conceito de imutabilidade, para adicionar algo em um array, preciso copiar o que já tem e depois adicionar. com o ... eu copio.
+            { week_day: 1, from: '', to: '' }
+        ]);
+    };
+
     return (
-        <div id="page-teacher-list" className="container">
-        <PageHeader title="Que incrível que você quer dar aulas." />
-    </div>
+        <div id="page-teacher-form" className="container">
+
+            <PageHeader
+                title="Que incrível que você quer dar aulas."
+                description="O primeiro passo é preencher este formulário de inscrição."
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+
+                    <Input name="name" label="Nome completo" />
+                    <Input name="avatar" label="Avatar" />
+                    <Input name="whatsapp" label="Whatsapp" />
+                    <Textarea name="bio" label="Biografia" />
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+
+                    <Select
+                        name="subject"
+                        label="Matéria"
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Histótia', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Inglês', label: 'Inglês' }
+                        ]}
+                    />
+
+                    <Input name="cost" label="Custo da sua hora aula" />
+
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                    <button type="button" onClick={addNewScheduleItem}>+ Novo horário</button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select
+                                    name="week_day"
+                                    label="Dia da semana"
+                                    options={[
+                                        { value: '0', label: 'Domingo' },
+                                        { value: '1', label: 'Segunda-feira' },
+                                        { value: '2', label: 'Terça-feira' },
+                                        { value: '3', label: 'Quarta-feira' },
+                                        { value: '4', label: 'Quinta-feira' },
+                                        { value: '5', label: 'Sexta-feira' },
+                                        { value: '6', label: 'Sábado' },
+                                    ]}
+                                />
+                                <Input name="from" label="Das" type="time" />
+                                <Input name="to" label="Até" type="time" />
+                            </div>
+                        );
+                    })}
+                </fieldset>
+
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante" />
+                    Importante! <br />
+                    Preencha todos os dados!
+                </p>
+
+                    <button type="button">
+                        Salvar cadastro
+                </button>
+                </footer>
+            </main>
+        </div>
     )
 };
 
