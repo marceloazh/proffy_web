c0a0a9a02b028f4bbffeda097d7278f6df718101
Marcelo Porto <marcelo.portos@gmail.com>
2020-08-09 22:49:14 -0300

    usando a API pela primeira vez no front
    usando a API pela primeira vez no front, no total de conexões, logo na landing page.

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
 import { Link } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
@@ -11,7 +12,19 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 
 
+
 function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    //aqui vai 2 parametros: uma função e na segunda posição parametros para iniciar aquela função. Se quero que a função execute apenas uma vez, deixo o array vazio.
+    useEffect(() => {
+        api.get('/connections').then(response => {
+            const { total } = response.data; //usando desestruturação, para pegar o total dentro de response.data
+
+            setTotalConnections(total);
+        });
+    }, []); 
+    
     return (
         <div id="page-landing">
             <div id="page-landing-content" className="container">
@@ -39,7 +52,7 @@ function Landing() {
                 </div>
 
                 <span className="total-connections">
-                    Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
+                    Total de {totalConnections} conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo" />
                 </span>
             </div>
         </div>
